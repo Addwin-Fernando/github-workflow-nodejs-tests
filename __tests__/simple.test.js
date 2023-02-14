e27d562759e775acc4f6c2930bc16546f2fef096
Addwin Fernando <addwinf07@gmail.com>
2023-02-14 11:48:58 +0530

    Update simple.test.js

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,9 +1,9 @@
 test('1 + 1 = 2', () => {
-  expect(1 + 1).toBe(3);
+  expect(1 + 1).toBe(2);
 });
 
 test('1 + 3 = 4',() =>{
-  expect(1 + 3).toBe(5)
+  expect(1 + 3).toBe(4)
 }
 
 test('1 + 2 = 3', () => {
